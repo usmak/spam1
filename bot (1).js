4d13f8325faa6af52761e0444f5105b6760813cc
usmak <44900364+usmak@users.noreply.github.com>
2018-11-09 15:06:44 +0100

    Update bot (1).js

--- bot (1).js
+++ bot (1).js
@@ -7,8 +7,8 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "509357857666236430"; // ايدي السررفر
-var channel = "509357857666236434";//ايدي الروم
+var server = "510453176185389076"; // ايدي السررفر
+var channel = "510453176185389078";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
     },305);
